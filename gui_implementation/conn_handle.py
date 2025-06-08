88a163f06b0a11c5efdacc09df66ef8fffc48b4b
Sofia <sofia.moumont@gmail.com>
2025-06-08 16:54:17 -0300

    Construção conn_handle.py

---
+++ gui_implementation/conn_handle.py
@@ -0,0 +1,41 @@
+import psycopg2
+import config
+
+class DBConnection:
+    connection = None
+
+    def __new__(classe):
+        if classe.connection is None:
+            classe.connection = super().__new__(classe)
+        return classe.connection
+
+    def _connect(self):
+        try:
+            return psycopg2.connect(
+                host=config.host,
+                database=config.name,
+                user=config.user,
+                password=config.pass
+            )
+        except Exception as e:
+            raise ConnectionError(f"Falha na conxão: {e}")
+
+    def execute(self, query, params=None, fetch=False):
+        conn = None
+        try:
+            conn = self._connect()
+            conn.cursor().execute(query, params or ())
+            if fetch:
+                return conn.cursor().fetchall()
+                conn.commit()
+        except Exception as e:
+            if conn:
+                conn.rollback()
+            raise RuntimeError(f"Query falhou: {e}")
+        finally:
+            if conn:
+                conn.close()
+
+
+db = DBConnection()
+
