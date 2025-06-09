3b92543d66c8d290841b080f918f2a6bf623eb22
Nicholas2104 <nicholasnogueira.costa@gmail.com>
2025-06-08 21:37:52 -0300

    simple button fucntionality

--- gui_implementation/main.py
+++ gui_implementation/main.py
@@ -13,30 +13,41 @@ class CRUD_Menu(tk.Tk):
         subtitle_font = tkFont.Font(family="Aptos",size=10)
 
 
-        self.title_label = tk.Label(text="STEAM",fg="white",bg="#1F1F1F",font=title_font)
+        self.title_label = tk.Label(master=self, text="STEAM",fg="white",bg="#1F1F1F",font=title_font)
         self.title_label.place(x=585,y=70)
 
-        self.subtitle_label = tk.Label(text="SISTEMA DE GERENCIAMENTO DE BANCO DE DADOS",fg="white",bg="#1F1F1F",font=subtitle_font)
+        self.subtitle_label = tk.Label(master=self, text="SISTEMA DE GERENCIAMENTO DE BANCO DE DADOS",fg="white",bg="#1F1F1F",font=subtitle_font)
         self.subtitle_label.place(x=527,y=150)
 
-        self.navto_create_btt = tk.Button(text="Inserir um novo registro",padx=220,pady=10,font=bttn_font)
+        self.navto_create_btt = tk.Button(master=self, text="Inserir um novo registro",padx=220,pady=10,font=bttn_font,command=self.nav_create)
         self.navto_create_btt.place(x=300,y=195)
 
-        self.navto_read_btt = tk.Button(text="Ler Registros Atuais",padx=244,pady=10,font=bttn_font)
+        self.navto_read_btt = tk.Button(master=self, text="Ler Registros Atuais",padx=244,pady=10,font=bttn_font,command=self.nav_read)
         self.navto_read_btt.place(x=300,y=315)
 
-        self.navto_update_btt = tk.Button(text="Atualizar Registros",padx=255,pady=10,font=bttn_font)
+        self.navto_update_btt = tk.Button(master=self, text="Atualizar Registros",padx=255,pady=10,font=bttn_font,command=self.nav_update)
         self.navto_update_btt.place(x=300,y=435)
 
-        self.navto_delete_btt =tk.Button(text="Deletar Registros",padx=265,pady=10,font=bttn_font)
+        self.navto_delete_btt =tk.Button(master=self, text="Deletar Registros",padx=265,pady=10,font=bttn_font,command=self.nav_delete)
         self.navto_delete_btt.place(x=300,y=555)
 
-        print(self.get_width_height(self.title_label))
     def get_width_height(self,widget:tk.Widget):
         self.update()
         w = widget.winfo_width()
         h = widget.winfo_height()
         return w,h
+    
+    def nav_create(self):
+        print("create")
+
+    def nav_read(self):
+        print("read")
+
+    def nav_update(self):
+        print("up")
+
+    def nav_delete(self):
+        print("del")
 
 
 def main():
